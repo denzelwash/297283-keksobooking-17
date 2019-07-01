fbf5e3eca82bec0df17ea11428c636e7adc97da4
denzelwash <denzelwash33@mail.ru>
2019-07-01 21:49:24 +0300

    На проверку

--- js/data.js
+++ js/data.js
@@ -1,68 +1,42 @@
 'use strict';
 
 (function () {
-  var similars = [];
-  var images = [];
-  var types = ['palace', 'flat', 'house', 'bungalo'];
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
-  var SIMILAR_LENGTH = 8;
   var PIN_TEMPLATE = document.querySelector('#pin');
   var PIN_BLOCK = PIN_TEMPLATE.content.querySelector('.map__pin');
+  var ERROR_TEMPLATE = document.querySelector('#error');
+  var ERROR_BLOCK = ERROR_TEMPLATE.content.querySelector('.error');
   var similarsFragment = document.createDocumentFragment();
-
-  function getAvatar() {
-    var random = window.util.getRandom(images.length);
-    var imageNumber = images.splice(random, 1);
-    return 'img/avatars/user0' + imageNumber + '.png';
-  }
-
-  function getType() {
-    return types[window.util.getRandom(types.length)];
-  }
-
-  function getLocation(direction) {
-    if (direction === 'x') {
-      return window.util.getRandom(window.util.MAP.offsetWidth + 1);
-    }
-    return window.util.getRandom(window.util.MAX_Y_COORDS - window.util.MIN_Y_COORDS) + window.util.MIN_Y_COORDS + 1;
-  }
 
   function createStyle(obj) {
     return 'left: ' + (obj.x - PIN_WIDTH / 2) + 'px; top: ' + (obj.y - PIN_HEIGHT) + 'px;';
   }
 
-  function createPin(obj) {
-    var clonePin = PIN_BLOCK.cloneNode(true);
-    var location = obj.location;
-    clonePin.setAttribute('style', createStyle(location));
-    clonePin.children[0].setAttribute('src', obj.author.avatar);
-    clonePin.setAttribute('alt', obj.offer.type);
-    similarsFragment.appendChild(clonePin);
-  }
-
-  for (var i = 1; i <= SIMILAR_LENGTH; i++) {
-    images.push(i);
-  }
-
-  for (var k = 1; k <= SIMILAR_LENGTH; k++) {
-    similars.push({
-      'author': {
-        'avatar': getAvatar(),
-      },
-      'offer': {
-        'type': getType()
-      },
-      'location': {
-        'x': getLocation('x'),
-        'y': getLocation('y')
+  function successHandler(similars) {
+    for (var i = 0; i < similars.length; i++) {
+      if (!similars[i].offer) {
+        continue;
       }
+      var clonePin = PIN_BLOCK.cloneNode(true);
+      var location = similars[i].location;
+      clonePin.setAttribute('style', createStyle(location));
+      clonePin.children[0].setAttribute('src', similars[i].author.avatar);
+      clonePin.children[0].setAttribute('alt', similars[i].offer.type);
+      similarsFragment.appendChild(clonePin);
+    }
+  }
+
+  function errorHandler() {
+    var cloneError = ERROR_BLOCK.cloneNode(true);
+    var errorButton = cloneError.querySelector('.error__button');
+    window.util.MAIN_BLOCK.appendChild(cloneError);
+    errorButton.addEventListener('click', function () {
+      window.location.reload();
     });
   }
 
-  for (var j = 1; j <= SIMILAR_LENGTH; j++) {
-    createPin(similars[j - 1]);
-  }
+  window.load(successHandler, errorHandler);
 
   window.data = {
     similarsFragment: similarsFragment
